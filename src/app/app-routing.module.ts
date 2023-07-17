fe74376b403c87022c95ffebaa959f37af6a248a
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2023-07-17 00:19:35 -0500

    main modules routing and guard to priv routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { hasSessionLoad } from './utils/guards/session.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('src/app/modules/public/public.module').then(m => m.PublicModule),
+  },
+  {
+    path: 'priv',
+    loadChildren: () => import('src/app/modules/private/private.module').then(m => m.PrivateModule),
+    canMatch: [hasSessionLoad]
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
